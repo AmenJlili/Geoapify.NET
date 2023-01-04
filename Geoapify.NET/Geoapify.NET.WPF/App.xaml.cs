676dd25d3597bdfcceef6e003dde70a35d5423c6
Amen Jlili <amen@bluebyte.biz>
2023-01-03 16:09:06 -0800

    Added beta version

---
+++ Geoapify.NET/Geoapify.NET.WPF/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace Geoapify.NET.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var ret = Geoapify.NET.AutocompleteHelper.GetAddress("8940 University");
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Geoapify.NET/Geoapify.NET.WPF/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Geoapify.NET.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.DataContext = new WindowViewModel();
+            InitializeComponent();
+        }
+    }
+
+
+    public class WindowViewModel : BindableBase
+    {
+
+        private ObservableCollection<Address> addresses = new ObservableCollection<Address>();
+        public ObservableCollection<Address> Addresses
+        {
+            get { return addresses; }
+            set { SetProperty(ref addresses, value); }
+        }
+
+        public Geoapify.NET.Address SelectedAddress { get; set; }
+
+        private string query;
+        public string Query
+        {
+            get { return query; }
+            set { SetProperty(ref query, value); }
+        }
+
+        private bool isBusy = true;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+
+        public WindowViewModel()
+        {
+            this.PropertyChanged += WindowViewModel_PropertyChanged;
+        }
+
+        private void WindowViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+
+            if (e.PropertyName == nameof(Query))
+            {
+
+                IsBusy = true; 
+
+                if (string.IsNullOrWhiteSpace(Query))
+                    Addresses.Clear();
+
+                var addresses = AutocompleteHelper.GetAddress(query);
+
+                if (addresses != null)
+                {
+                    var toBeRemovedAddresses = Addresses.Where(x => addresses.Select(j => j.formatted).Contains(x.formatted) == false).ToArray();
+                    var toBeAddedAddresses = addresses.ToList().Where(x => Addresses.Select(j => j.formatted).Contains(x.formatted) == false).ToArray();
+
+
+                    foreach (var toBeRemovedAddresse in toBeRemovedAddresses)
+                        Addresses.Remove(toBeRemovedAddresse);
+
+                    toBeAddedAddresses.ToList().ForEach(x => Addresses.Add(x));
+
+
+                    IsBusy = false;
+                }
+
+
+              
+            }
+        }
+    }
+}
